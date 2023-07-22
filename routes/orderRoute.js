8d1f241b82f37570870692148fe4190877d89b68
Farhan Shahid <123633657+farhan7479@users.noreply.github.com>
2023-07-22 10:11:08 +0530

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,7 +5,7 @@ const path = require("path");
 const orderController = require('../controllers/orderController.js');
 
 router.use(express.static(path.resolve(__dirname, 'public')));
-router.use(express.json());
+
 
 // Configure Multer for file upload
 const storage = multer.diskStorage({
